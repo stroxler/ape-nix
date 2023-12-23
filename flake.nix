c504549aae82f8e8450b54a09acd5a719be5299f
Steven Troxler <steven.troxler@gmail.com>
2023-12-23 09:54:36 -0800

    Initial home-manager config is working.
    It took a while to get here, but I've now got home manager working
    and controlling a bare-bones git setup, which I'm using to write
    this commit :)
    
    I'm also adding `flake.lock` to version control, although at
    this point there are issues; home manager isn't happy that I'm using
    mismatched versions of home-manager vs nixpkgs so I need to fix
    that and update the lock.

--- flake.nix
+++ flake.nix
@@ -7,10 +7,39 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = inputs: let
+    username = "stroxler";
+    machine = "trox-mbp-2023-12";
     system = "aarch64-darwin";
     pkgs = import inputs.nixpkgs {inherit system;};
   in {
     defaultPackage.${system} = inputs.home-manager.defaultPackage.${system};
-
+    homeConfigurations.${username} = inputs.home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [
+        {
+          home = {
+            stateVersion = "23.11";
+            username = "${username}";
+            homeDirectory = "/Users/${username}";
+            packages = [
+              pkgs.cowsay
+            ];
+          };
+          programs = {
+            home-manager = {
+              enable = true;
+            };
+            git = {
+              enable = true;
+              userName = "Steven Troxler";
+              userEmail = "steven.troxler@gmail.com";
+              aliases = {
+                check = "checkout";
+              };
+            };
+          };
+        }
+      ];
+    };
   };
 }
