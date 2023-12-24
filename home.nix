0b12c01fe07b8e0425294d23941f6275bb25f45b
Steven Troxler <steven.troxler@gmail.com>
2023-12-24 12:54:47 -0800

    Break my home config into multiple modules

--- home.nix
+++ home.nix
@@ -8,14 +8,25 @@
 make-homeConfiguration {
   inherit pkgs;
   modules = [
+    # Basic metadata
     {
       home = {
         stateVersion = "23.11";
         username = "${username}";
         homeDirectory = "/Users/${username}";
+      };
+    }
+    # Profile packages not managed as programs
+    {
+      home = {
         packages = [
           pkgs.cowsay
         ];
+      };
+    }
+    # Dotfiles not managed via nix configs
+    {
+      home = {
         file = {
           # Note: .inputrc only affects readline using tools, which includes
           # bash but not zsh (bindkey directives control this in zsh).
@@ -25,36 +36,42 @@ make-homeConfiguration {
           '';
         };
       };
+    }
+    {
       programs = {
         home-manager = {
           enable = true;
         };
-        git = {
+      };
+    }
+    {
+      programs.git = {
+        enable = true;
+        userName = "Steven Troxler";
+        userEmail = "steven.troxler@gmail.com";
+        aliases = {
+          check = "checkout";
+        };
+        ignores = [
+          "result"
+        ];
+      };
+    }
+    {
+      programs.zsh = {
+        enable = true;
+        antidote = {
           enable = true;
-          userName = "Steven Troxler";
-          userEmail = "steven.troxler@gmail.com";
-          aliases = {
-            check = "checkout";
-          };
-          ignores = [
-            "result"
+          plugins = [
+            "zsh-users/zsh-syntax-highlighting"
+            "zsh-users/zsh-autosuggestions"
+            "zsh-users/zsh-history-substring-search"
           ];
         };
-        zsh = {
-          enable = true;
-          antidote = {
-            enable = true;
-            plugins = [
-              "zsh-users/zsh-syntax-highlighting"
-              "zsh-users/zsh-autosuggestions"
-              "zsh-users/zsh-history-substring-search"
-            ];
-          };
-          envExtra = ''
-            bindkey '^[[A' history-substring-search-up # or '\eOA'
-            bindkey '^[[B' history-substring-search-down # or '\eOB'
-          '';
-        };
+        envExtra = ''
+          bindkey '^[[A' history-substring-search-up # or '\eOA'
+          bindkey '^[[B' history-substring-search-down # or '\eOB'
+        '';
       };
     }
   ];
