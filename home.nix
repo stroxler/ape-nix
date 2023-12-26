fd1649bdca8f033aa6aa8113aee7062f7b0724ea
Steven Troxler <steven.troxler@gmail.com>
2023-12-26 07:28:35 -0800

    Finish porting git config from dotconfar

--- home.nix
+++ home.nix
@@ -21,6 +21,7 @@ make-homeConfiguration {
       home = {
         packages = [
           pkgs.cowsay
+          pkgs.neovim
         ];
       };
     }
@@ -29,6 +30,7 @@ make-homeConfiguration {
       home = {
         file = {
           ".config/starship.toml".source = ./dotfiles/starship.toml;
+          ".config/gitignore.conf".source = ./dotfiles/gitignore.conf;
           # Note: .inputrc only affects readline using tools, which includes
           # bash but not zsh (bindkey directives control this in zsh).
           ".inputrc".text = ''
@@ -51,10 +53,17 @@ make-homeConfiguration {
         userEmail = "steven.troxler@gmail.com";
         aliases = {
           check = "checkout";
+          lg = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(blue)%s%C(reset) - %an%C(reset)%C(bold blue)%d%C(reset)' --all";
         };
         ignores = [
           "result"
         ];
+        extraConfig = {
+          core = {
+            editor = "nvim";
+            excludesFile = "~/.config/gitignore.conf";
+          };
+        };
       };
     }
     {
