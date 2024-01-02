939d872f5268f04229f3e319e1f4fbed3d0f8d11
Steven Troxler <steven.troxler@gmail.com>
2024-01-02 14:02:09 -0800

    Make a stand-alone nix-darwin setup

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,54 +2,134 @@
   description = "stroxler system setup with nix";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgsUnstable.url = "github:NixOS/nixpkgs/master";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-darwin = {
       url = "github:lnl7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs =
-    { self
-    , nixpkgs
-    , nix-darwin
-    , ...
-    }:
-    let
-      username = "stroxler";
-      pkgs-for-system = system: import nixpkgs {
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgsUnstable,
+    home-manager,
+    nix-darwin,
+    ...
+  }: let
+    username = "stroxler";
+    pkgs-for-system = system:
+      import nixpkgs {
         inherit system;
 
         # needed mainly for Microsoft fonts
-        config = { allowUnfree = true; };
+        config = {allowUnfree = true;};
       };
-      make-packages = system:
-        let pkgs = pkgs-for-system system; in {
-          packages.${system} = {
-            darwin-rebuild = nix-darwin.packages.${system}.default;
-            default = self.${system}.packages.darwin-rebuild;
-          };
-        };
-      make-darwin-configuration = system: owner:
-        if (system != "x86_64-darwin" && system != "aarch64-darwin")
-        then abort "nix-darwin can only be used on darwin systems"
-        else
-          let configuration-name = "${system}--${owner}"
-          let pkgs = pkgs-for-system system; in {
-            darwinConfigurations.${configuration-name} = import ./darwin.nix {
-              inherit system;
-              inherit owner;
-              inherit username;
-              inherit pkgs;
-              make-darwinConfiguration =
-                nix-darwin.lib.darwinSystem;
+    nix-darwin-bin = system:
+      if (system != "x86_64-darwin" && system != "aarch64-darwin")
+      then abort "nix-darwin is only usable on darwin systems"
+      else let
+        pkgs = pkgs-for-system system;
+      in {
+        packages.${system}.default = nix-darwin.packages.${system}.default;
+      };
+    combine-configs = configs: nixpkgs.lib.foldr nixpkgs.lib.recursiveUpdate {} configs;
+    nix-darwin-config = system: owner: let
+      pkgs = pkgs-for-system system;
+      configuration-name = "${system}--${owner}";
+    in {
+      darwinConfigurations.${configuration-name} = nix-darwin.lib.darwinSystem {
+        inherit system;
+        inherit pkgs;
+        modules = [
+          {
+            system.stateVersion = 4;
+            system.defaults = {
+              trackpad.TrackpadThreeFingerDrag = true;
+              dock.autohide = true;
+              finder = {
+                AppleShowAllExtensions = true;
+                _FXShowPosixPathInTitle = true;
+              };
+              NSGlobalDomain = {
+                AppleShowAllExtensions = true;
+                InitialKeyRepeat = 14;
+                KeyRepeat = 5;
+              };
             };
-          };
-      combine-configs = configs: nixpkgs.lib.foldr nixpkgs.lib.recursiveUpdate { } configs;
-    in
+            nix.useDaemon = true;
+            nix.extraOptions = ''
+              experimental-features = nix-command flakes
+            '';
+            documentation.enable = true;
+            environment = {
+              shells = with pkgs; [bash zsh];
+              loginShell = pkgs.zsh;
+              systemPackages = [pkgs.coreutils];
+              systemPath = ["/opt/homebrew/bin"];
+              pathsToLink = ["/Applications"];
+            };
+            fonts = {
+              fontDir.enable = true; # only allow nix to control fonts
+              fonts = [
+                (pkgs.nerdfonts.override {
+                  fonts = [
+                    "FiraCode"
+                    "Hasklig"
+                    "DroidSansMono"
+                    "DejaVuSansMono"
+                    "iA-Writer"
+                    "JetBrainsMono"
+                    "Meslo"
+                    "SourceCodePro"
+                    "Inconsolata"
+                    "NerdFontsSymbolsOnly"
+                  ];
+                })
+              ];
+            };
+            homebrew = {
+              # nix-darwin prevents cli use of brew, so to search use
+              # the website at https://formulae.brew.sh/
+              enable = true;
+              caskArgs.no_quarantine = true;
+              global.brewfile = true;
+              masApps = {};
+              casks =
+                [
+                  "rectangle"
+                  "clipy"
+                  "emacs"
+                  "firefox"
+                  "obs"
+                  "spotify"
+                ]
+                ++ (
+                  # Some programs are IT-managed on my work machine, so exclude them.
+                  if (owner == "work")
+                  then []
+                  else [
+                    "iterm2"
+                    "google-crhome"
+                    "visual-studio-code"
+                    "zoom"
+                  ]
+                );
+            };
+            security.pam.enableSudoTouchIdAuth = true;
+          }
+        ];
+      };
+    };
+  in
     combine-configs [
-      (make-packages "x86_64-darwin")
-      (make-packages "aarch64-darwin")
-      (make-darwin-configuration "x86_64-darwin" "me")
-      (make-darwin-configuration "aarch64-darwin" "me")
-      (make-darwin-configuration "aarch64-darwin" "work")
+      (nix-darwin-bin "x86_64-darwin")
+      (nix-darwin-bin "aarch64-darwin")
+      (nix-darwin-config "x86_64-darwin" "me")
+      (nix-darwin-config "aarch64-darwin" "me")
+      (nix-darwin-config "aarch64-darwin" "work")
     ];
 }
