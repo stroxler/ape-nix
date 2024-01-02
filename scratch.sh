65c3b95b2c14715230862df845eea6f66d303a6f
Steven Troxler <steven.troxler@gmail.com>
2024-01-02 14:03:26 -0800

    Remove unused files

--- scratch.sh
+++
@@ -1,1 +0,0 @@
- git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(blue)%s%C(reset) - %an%C(reset)%C(bold blue)%d%C(reset)' --all
