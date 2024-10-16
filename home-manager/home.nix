7ec4e9c92bbabb36fd927c8bc6257771f79536b3
Steven Troxler <steven.troxler@gmail.com>
2024-10-16 14:00:49 -0700

    Try out another wezterm color theme

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -140,7 +140,7 @@ in
             ".wezterm.lua".text = ''
               local wezterm = require 'wezterm'
               local config = wezterm.config_builder()
-              config.color_scheme = 'Sea Shells (Gogh)'
+              config.color_scheme = 'Aci (Gogh)'
               config.keys = {
                 {
                   key = 'd',
