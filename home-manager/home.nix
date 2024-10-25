d0bda78fd3e3952dfe3e7b86eea104ce3792b4ff
Steven Troxler <steven.troxler@gmail.com>
2024-10-24 18:56:31 -0700

    Try out another wezterm color theme

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -140,7 +140,7 @@ in
             ".wezterm.lua".text = ''
               local wezterm = require 'wezterm'
               local config = wezterm.config_builder()
-              config.color_scheme = 'Alien Blood (Gogh)'
+              config.color_scheme = 'Arthur (Gogh)'
               config.keys = {
                 {
                   key = 'd',
