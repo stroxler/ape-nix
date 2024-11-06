a33d39e0f3e7afcd2c24248b8beee497ca6aa46e
Steven Troxler <steven.troxler@gmail.com>
2024-11-06 05:13:35 -0800

    Try out another wezterm color theme

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -140,7 +140,7 @@ in
             ".wezterm.lua".text = ''
               local wezterm = require 'wezterm'
               local config = wezterm.config_builder()
-              config.color_scheme = 'Bamboo'
+              config.color_scheme = 'Chalk'
               config.keys = {
                 {
                   key = 'd',
