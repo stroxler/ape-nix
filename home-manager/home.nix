1818b6ef356c2e371ae1ef30b2ab7612db855e02
Steven Troxler <steven.troxler@gmail.com>
2024-11-07 16:37:48 -0800

    Try out another wezterm color theme

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -140,7 +140,7 @@ in
             ".wezterm.lua".text = ''
               local wezterm = require 'wezterm'
               local config = wezterm.config_builder()
-              config.color_scheme = 'Chalk'
+              config.color_scheme = 'Jellybeans'
               config.keys = {
                 {
                   key = 'd',
