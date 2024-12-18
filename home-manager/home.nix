0246d27912573081af3f7796788a02319c03002c
Steven Troxler <steven.troxler@gmail.com>
2024-12-18 10:34:17 -0800

    Try out another wezterm color theme

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -140,7 +140,7 @@ in
             ".wezterm.lua".text = ''
               local wezterm = require 'wezterm'
               local config = wezterm.config_builder()
-              config.color_scheme = 'Lumifoo (terminal.sexy)'
+              config.color_scheme = 'MaterialDesignColors'
               config.keys = {
                 {
                   key = 'd',
